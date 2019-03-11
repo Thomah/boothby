8bcf7b7fd6ff69666e72c8b3f3c94b442658751a
jimmydore <jimmy291295@gmail.com>
2019-03-11 16:11:41 +0100

    added users table which list users with buttons to delete them

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,11 +1,88 @@
-//When we press the enter button, we trigger the login button
-document.getElementById("password")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("login").click(); // or just login()
+//When we press the enter button on the auth page, we trigger the login button
+if (document.getElementById("password")){
+  document.getElementById("password")
+      .addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+          document.getElementById("login").click(); // or just login()
+      }
+  });
+}
+
+function refresh(){
+  overload_xhr(
+    "GET",
+    "/api/user",
+    function(xhr){
+      var json = JSON.parse(xhr.responseText);
+      fill_users_table(json);
     }
-});
+  );
+}
+
+function fill_users_table(users) {
+  var usersTable = document
+  .getElementById("users")
+  .getElementsByTagName("tbody")[0];
+
+  usersTable.style.display = "none";
+
+  // Delete previous entries
+  var rowCount = usersTable.childNodes.length;
+  for (var x = rowCount - 1; x >= 0; x--) {
+    usersTable.removeChild(usersTable.childNodes[x]);
+  }
+
+  var userId, user, newEntry, button, cell, cellSpan;
+
+  for (userId in users){
+    user = users[userId];
+    newEntry = document.createElement("tr");
+    newEntry.id = user._id;
+
+    //Username
+    cell = document.createElement("td");
+    cell.textContent = user.username;
+    newEntry.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellSpan = document.createElement("span");
+
+    //Remove button
+    button = document.createElement("button");
+    button.appendChild(document.createTextNode("Remove user"));
+    button.onclick = remove_user;
+    cellSpan.appendChild(button);
+
+    cell.appendChild(cellSpan);
+    newEntry.appendChild(cell);
+    usersTable.appendChild(newEntry)
+  }
+
+  usersTable.style.display = "table-row-group";
+}
+
+var remove_user = function remove_user() {
+  var textButton = this.firstChild.parentElement;
+  var row = textButton.parentElement.parentElement.parentElement;
+  var table = row.parentElement;
+
+  var par_error = document.getElementById("cant-remove-user");
+
+  overload_xhr(
+    "DELETE",
+    `/api/user/${row.id}`,
+    function(){
+      table.removeChild(row);
+      par_error.style.display = "none";
+    },
+    function(){},
+    function(){
+      par_error.style.display = "block";
+    }
+  );
+};
+
 
 function login() {
   var user = document.getElementById("username").value;
@@ -48,12 +125,16 @@ function add_user() {
   var par_success = document.getElementById("user-created");
   var par_not_success = document.getElementById("user-not-created");
 
+  var par_error = document.getElementById("cant-remove-user");
+
   overload_xhr(
     "POST",
     "/api/user",
     function(){
       par_success.style.display = "block";
       par_not_success.style.display = "none";
+      par_error.style.display = "none";
+      refresh();
     },
     function(xhr){
       xhr.setRequestHeader('user', user);
@@ -62,6 +143,8 @@ function add_user() {
     function(){
       par_success.style.display = "none";
       par_not_success.style.display = "block";
+      par_error.style.display = "none";
     },
   );
+
 }
--- src/users.js
+++ src/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const NodeCache = require("node-cache");
 const api = require("./api.js");
+const db = require("./db.js");
 
 var myCache;
 var ttlCache = 36000;
@@ -38,95 +39,117 @@ var route = function (request, response) {
         password: request.headers.pwd
     };
 
+    var regex_user = /^\/api\/user\/([^/]+)$/
+
     // GET : retrieve existing user, and send back the username and the token
     //       used during authentication
-    if (request.method === "POST") {
-
-        if (request.url === "/api/user/login") {
-            //FIXME : The password can be seen in the request header, should be crypted in the client side
-            api.checkCredentialsUser(credentials, function (data) {
-                if (data != false) { //if the user exists
-                    bcrypt.compare(credentials['password'], data['password'], function (err, res) {
-                        if (res == true) {
-                            var generated_token = generate_token();
-                            //We get the array tokens in the server cache
-                            myCache.get("tokens", function (err, value) {
-                                if (!err) {
-                                    if (value == undefined) {
-                                        //The array in the cache does not exist : Init of the array containing the tokens
-                                        myCache.set("tokens", [generated_token], function (err) {
-                                            if (err) {
-                                                response.writeHead(201, { "Content-Type": "application/json" });
-                                                response.end();
-                                            }
-                                        });
-                                    } else {
-                                        //The array tokens already exists in the cache
-                                        var tokens = value;
-                                        tokens.push(generated_token);
-                                        //Already a token in the server, we add the new generated token in the tokens cache array
-                                        myCache.set("tokens", tokens, function (err) {
-                                            if (err) {
-                                                response.writeHead(201, { "Content-Type": "application/json" });
-                                                response.end();
-                                            }
-                                        });
-                                    }
+    if (request.url === "/api/user/login") {
+        //FIXME : The password can be seen in the request header, should be crypted in the client side
+        api.checkCredentialsUser(credentials, function (data) {
+            if (data != false) { //if the user exists
+                bcrypt.compare(credentials['password'], data['password'], function (err, res) {
+                    if (res == true) {
+                        var generated_token = generate_token();
+                        //We get the array tokens in the server cache
+                        myCache.get("tokens", function (err, value) {
+                            if (!err) {
+                                if (value == undefined) {
+                                    //The array in the cache does not exist : Init of the array containing the tokens
+                                    myCache.set("tokens", [generated_token], function (err) {
+                                        if (err) {
+                                            response.writeHead(201, { "Content-Type": "application/json" });
+                                            response.end();
+                                        }
+                                    });
+                                } else {
+                                    //The array tokens already exists in the cache
+                                    var tokens = value;
+                                    tokens.push(generated_token);
+                                    //Already a token in the server, we add the new generated token in the tokens cache array
+                                    myCache.set("tokens", tokens, function (err) {
+                                        if (err) {
+                                            response.writeHead(201, { "Content-Type": "application/json" });
+                                            response.end();
+                                        }
+                                    });
                                 }
-                            });
-                            response.writeHead(200, { "Content-Type": "application/json" });
-                            response.end(JSON.stringify({ token: generated_token }));
-                        } else {
-                            response.writeHead(201, { "Content-Type": "application/json" });
-                            response.end();
-                        }
-                    });
-                } else {// The username does not exist in the DB
-                    //FIXME : If no user in database, which status code should I return ?
-                    response.writeHead(201, { "Content-Type": "application/json" });
-                    response.end();
-                }
-            });
-        }
+                            }
+                        });
+                        response.writeHead(200, { "Content-Type": "application/json" });
+                        response.end(JSON.stringify({ token: generated_token }));
+                    } else {
+                        response.writeHead(201, { "Content-Type": "application/json" });
+                        response.end();
+                    }
+                });
+            } else {// The username does not exist in the DB
+                //FIXME : If no user in database, which status code should I return ?
+                response.writeHead(201, { "Content-Type": "application/json" });
+                response.end();
+            }
+        });
+    }
 
-        else if (request.url === "/api/user/logout") {
-            myCache.get("tokens", function (err, value) {
-                if (!err) {
-                    //We remove this specific token from the server cache
-                    var tokens = value;
-                    var index_token_to_remove = tokens.indexOf(request.headers.token);
-                    tokens.splice(index_token_to_remove, 1);
-                    myCache.set("tokens", tokens, function (err) {
-                        if (err) {
-                            response.writeHead(201, { "Content-Type": "application/json" });
-                            response.end();
-                        }
-                    });
-                }
-            });
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.end();
-        }
-
-
-        else if (request.url === "/api/user") {
-            if (request.method === "POST") {
-                //FIXME : The password can be seen in the request header, should be crypted in the client side
-                //See usage of bcrypt library : https://www.npmjs.com/package/bcrypt
-                bcrypt.hash(credentials['password'], saltRounds, function (err, hash) {
-                    credentials['password'] = hash;
-                    api.addUser(credentials, function (data) {
-                        if (data == false) {//User already existing
-                            //FIXME : Status code
-                            response.writeHead(201, { "Content-Type": "application/json" });
-                            response.end();
-                        } else {
-                            response.writeHead(200, { "Content-Type": "application/json" });
-                            response.end();
-                        }
-                    });
+    else if (request.url === "/api/user/logout") {
+        myCache.get("tokens", function (err, value) {
+            if (!err) {
+                //We remove this specific token from the server cache
+                var tokens = value;
+                var index_token_to_remove = tokens.indexOf(request.headers.token);
+                tokens.splice(index_token_to_remove, 1);
+                myCache.set("tokens", tokens, function (err) {
+                    if (err) {
+                        response.writeHead(201, { "Content-Type": "application/json" });
+                        response.end();
+                    }
                 });
             }
+        });
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end();
+    }
+
+
+    else if (request.url === "/api/user") {
+        if (request.method === "POST") {
+            //FIXME : The password can be seen in the request header, should be crypted in the client side
+            //See usage of bcrypt library : https://www.npmjs.com/package/bcrypt
+            bcrypt.hash(credentials['password'], saltRounds, function (err, hash) {
+                credentials['password'] = hash;
+                api.addUser(credentials, function (data) {
+                    if (data == false) {//User already existing
+                        //FIXME : Status code
+                        response.writeHead(201, { "Content-Type": "application/json" });
+                        response.end();
+                    } else {
+                        response.writeHead(200, { "Content-Type": "application/json" });
+                        response.end();
+                    }
+                });
+            });
+        }else if (request.method === "GET") {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            db.list("user", function (data) {
+                response.write(JSON.stringify(data));
+                response.end();
+            });
+        }
+    }else if (request.url.match(regex_user) !== null) {
+        var userId = request.url.match(regex_user)[1];
+        // DELETE : delete a dialog
+        if (request.method === "DELETE") {
+            db.list("user", function (data) {
+                if(data.length <= 1){ //Last user in DB
+                    response.writeHead(402, { "Content-Type": "application/json" });
+                    response.end();
+                }else{
+                    db.delete("user", userId, function (data) {
+                        response.writeHead(200, { "Content-Type": "application/json" });
+                        response.write(JSON.stringify(data));
+                        response.end();
+                    });
+                }
+            });
         }
     }
 
@@ -143,7 +166,6 @@ var createDefaultUser = function(){
             'username':ADMIN_USERNAME,
             'password':hash
         }
-        console.log(credentials);
         api.createDefaultUser(credentials, function (data) {
             if (data == false) {//Already users in the database
                 console.log('No need to create default admin : already users in database ');
