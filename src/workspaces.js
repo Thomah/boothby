01337610459fcc0e6c1a770d1d5cafed7796e7e2
Thomah <5090230+Thomah@users.noreply.github.com>
2019-04-24 14:27:35 +0200

    First draft on workspace users page

--- src/workspaces.js
+++ src/workspaces.js
@@ -8,8 +8,50 @@ var response404 = function (response) {
 
 var route = function (request, response) {
 
+    var regex_workspaceId = /^\/api\/workspaces\/([^/]+)\/?$/;
+    var regex_workspaceIdReload = /^\/api\/workspaces\/([^/]+)\/reload\/?$/;
+
+    if (request.url.match(regex_workspaceId) !== null) {
+
+        var objectId = request.url.match(regex_workspaceId)[1];
+
+        // GET : Detail of workspace
+        if(request.method === "GET") {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            db.read("workspaces", { _id: new db.mongodb().ObjectId(objectId) }, function (data) {
+                response.write(JSON.stringify(data));
+                response.end();
+            });
+        }
+
+        // DELETE : revoke a workspace token
+        else if (request.method === "DELETE") {
+            db.read("workspaces", { _id: new db.mongodb().ObjectId(objectId) }, function(workspace) {
+                slack.revokeToken({
+                    user_access_token: workspace.access_token,
+                    bot_access_token: workspace.bot.bot_access_token
+                    });
+            });
+            db.delete("workspaces", objectId, function () {
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.end();
+            });
+        }
+
+        else {
+            response404(response);
+        }
+    }
+
+    else if (request.url.match(regex_workspaceIdReload) !== null) {
+        console.log("Est-ce que tu m'entends hého ?");
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.write("{}");
+        response.end();
+    }
+
     // GET : retrieve workspaces
-    if (request.method === "GET") {
+    else if (request.method === "GET") {
         response.writeHead(200, { "Content-Type": "application/json" });
         db.list("workspaces", function (data) {
             response.write(JSON.stringify(data));
@@ -17,27 +59,7 @@ var route = function (request, response) {
         });
     }
 
-    // DELETE : revoke a workspace token
-    else if (request.method === "DELETE") {
-        var regex_delete = /^\/api\/workspaces\/([^/]+)\/?$/;
-        if (request.url.match(regex_delete) !== null) {
-            var objectId = request.url.match(regex_delete)[1];
-            db.read("workspaces", { _id: new db.mongodb().ObjectId(objectId) }, function(workspace) {
-                slack.revokeToken({
-                    user_access_token: workspace.access_token,
-                    bot_access_token: workspace.bot.bot_access_token
-                  });
-            });
-            db.delete("workspaces", objectId, function (_result) {
-                response.writeHead(200, { "Content-Type": "application/json" });
-                response.end();
-            });
-        } else {
-            response404(response);
-        }
-    }
-
-    // Any other verb
+    // Any other case
     else {
         response404(response);
     }
