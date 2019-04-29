4efb68e796c58b9720d8975557c0a870330195ec
Thomah <5090230+Thomah@users.noreply.github.com>
2019-04-29 14:08:30 +0200

    Small adaptations about new dialog structure

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 var log = function(message) {
+    console.log(message);
     fs.appendFile('console.log', message + '\n', function (err) {
         if (err) throw err;
     });
