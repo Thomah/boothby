f3a0fb6040b4d18c1ed428b9a6aa683c5c984232
Thomah <5090230+Thomah@users.noreply.github.com>
2019-05-02 11:25:26 +0200

    Format logs

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
-var log = function(message) {
+var log = function (message) {
+    message = '[' + new Date().toLocaleString("FR", { timeZone: 'Europe/Paris' }) + '] ' + message;
     console.log(message);
     fs.appendFile('console.log', message + '\n', function (err) {
         if (err) throw err;
