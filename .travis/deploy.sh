5bde009b94c7307bb93b3d6557eb5c002dbaf38e
Thomah <5090230+Thomah@users.noreply.github.com>
2019-05-20 13:17:09 +0200

    Add ssh-keyscan to solve authenticity issues

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,9 +10,10 @@ git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
+ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
 ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
-    echo "$(date -u) Deploy 'Boothby'"  >> ./deploy.log
     cd $DEPLOY_DIR
+    echo "$(date -u) Travis Deploy"  >> ./console.log
     sudo service boothby reload
     exit
 EOT
