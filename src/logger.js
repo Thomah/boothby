8e6d60c4d2904e99a496433d20d791845f845f20
Thomah <5090230+Thomah@users.noreply.github.com>
2023-10-24 17:06:01 +0200

    Disable writing logs to file

--- src/logger.js
+++ src/logger.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 
+const SAVE_LOGS = process.env.SAVE_LOGS ? process.env.SAVE_LOGS : false;
+
 var print = function (level, message) {
     message = '[' + new Date().toLocaleString("FR", { timeZone: 'Europe/Paris' }) + '][' + level + '] ' + message;
     // eslint-disable-next-line no-console
     console.log(message);
-    fs.appendFile('console.log', message + '\n', function (err) {
-        if (err) throw err;
-    });
+    if(SAVE_LOGS) {
+        fs.appendFile('console.log', message + '\n', function (err) {
+            if (err) throw err;
+        });
+    }
 }
 
 exports.debug = function(message) {
